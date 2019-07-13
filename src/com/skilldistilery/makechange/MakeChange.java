e03969c64a854d0ccbd11a417bab9e8328c7d4f8
JHowell78 <jahowell1978@gmail.com>
2019-07-13 14:32:16 -0600

    added formula for 20 dollar bills and fine tuned the text the user will see

--- src/com/skilldistilery/makechange/MakeChange.java
+++ src/com/skilldistilery/makechange/MakeChange.java
@@ -13,43 +13,46 @@ public class MakeChange
 
 		kb.close();
 	}
-
 	public static void doTransaction(Scanner input)
 	{
 		double price, cash, changetotal;
 		System.out.println("Hello there, how much is the price of the widget you are purchasing? ");
-		System.out.println("Plenter enter the price of your item in dollars ");
+		System.out.println("Plenter enter the price of your item in dollars and cents ");
 		price = input.nextDouble();
-
-		System.out.println("Great, how much money are you giving me? ");
+		System.out.println("Great, how much money are you giving me in dollars and cents please? ");
 		cash = input.nextDouble();
-		System.out.println("Thank you.");
 		changetotal = cash - price;
 
 		if (cash < price)
 		{
 			System.out.println("Sorry that is not enough money for the widget.");
-			doTransaction(input);
+			//doTransaction(input);
 		}
 		if (cash > price)
 		{
 			changeMethod(changetotal);
-			
+
 		} else if (cash == price)
 		{
-			System.out.println("Thanks Have a nice day!");
+			System.out.println("Exact change! Thank you and have a nice day!");
 		}
 	}
-	
 	public static void changeMethod(double changetotal)
 	{
-		int TenDollarBills = 0, FiveDollarBills = 0, DollarBills = 0;
+		int TwentyDollarBills = 0, TenDollarBills = 0, FiveDollarBills = 0, DollarBills = 0;
 		double Pennies = 0, Nickels = 0, Dimes = 0, Quarters = 0;
-		System.out.println("Thanks for your purchase here is your change");
-		System.out.print("And your change is:  $ ");
+		System.out.println("Thanks for your purchase ");
+		System.out.print("And your total change is: $ ");
 		System.out.printf("%.2f%n", changetotal);
 		changetotal = changetotal * 100 + .005; // convert all change into pennies and fixes penny remainder
-
+		TwentyDollarBills =(int) (changetotal / 2000);
+		changetotal = changetotal % 2000;
+		if (TwentyDollarBills > 1)
+		{
+			System.out.println(TwentyDollarBills + " twenty dollar bills, ");
+		}else if (TwentyDollarBills == 1) {
+			System.out.println(TwentyDollarBills + " twenty dollar bill, ");
+		}
 		TenDollarBills = (int) (changetotal / 1000);
 		changetotal = changetotal % 1000;
 		if (TenDollarBills > 0)
@@ -64,14 +67,16 @@ public class MakeChange
 		}
 		DollarBills = (int) (changetotal / 100);
 		changetotal = changetotal % 100;
-		if (DollarBills > 0)
+		if (DollarBills > 1)
 		{
 			System.out.println(DollarBills + " one dollar bills, ");
+		} else if (DollarBills == 1)
+		{
+			System.out.println(DollarBills + " one dollar bill, ");
 		}
 		Quarters = (int) (changetotal / 25);
 		changetotal = changetotal % 25;
 		if (Quarters > 0)
-
 		{
 			System.out.printf("%.0f", Quarters); // print f %f fixes double decimal points
 			System.out.println(" quarters");
