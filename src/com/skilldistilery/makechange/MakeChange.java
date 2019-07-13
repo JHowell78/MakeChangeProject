ec0078cf72e239e4938165afe20594bc459ae112
JHowell78 <jahowell1978@gmail.com>
2019-07-13 13:17:36 -0600

    added all change formulas, is working as expected

--- src/com/skilldistilery/makechange/MakeChange.java
+++ src/com/skilldistilery/makechange/MakeChange.java
@@ -9,8 +9,6 @@ public class MakeChange
 	{
 		Scanner kb = new Scanner(System.in);
 		
-		double pennies, nickels, dimes, quaters, dollars;
-		String penny, nickel, dime, quater, dollar;
 		
 		doTransaction(kb);
 		
@@ -21,7 +19,9 @@ public class MakeChange
 	public static void doTransaction(Scanner input)
 	{
 		//method for transaction
-		double price, cash,change;
+		int TenDollarBills = 0, FiveDollarBills = 0, DollarBills = 0;
+		double Pennies = 0, Nickels = 0, Dimes = 0, Quarters = 0;
+		double price, cash, changetotal;
 		System.out.println("Hello there, how much is the price of the widget you are purchasing? ");
 		price = input.nextDouble();
 		
@@ -35,11 +35,68 @@ public class MakeChange
 		}
 		if (cash > price) {
 			System.out.println("Thanks for your purchase here is your change");
-			//makechange
+			//makechange method
+			changetotal = cash - price;
+			 
+			System.out.print("And your change is:  $ ");
+			System.out.printf("%.2f%n",  changetotal);
+			changetotal = changetotal * 100 + .005;  // convert all change into pennies
+			
+				
+
+			TenDollarBills = (int) (changetotal /1000);
+			changetotal = changetotal % 1000;
+			if (TenDollarBills > 0)
+			{
+				System.out.println(TenDollarBills + " ten dollar bill, ");
+			}
+			FiveDollarBills = (int) (changetotal / 500);
+			changetotal = changetotal % 500; 
+			if (FiveDollarBills > 0)
+			{
+				System.out.println(FiveDollarBills + " five dollar bill, ");
+			}
+			DollarBills = (int) (changetotal / 100);
+			changetotal = changetotal  % 100; 
+			if (DollarBills > 0)
+			{
+				System.out.println(DollarBills + " one dollar bills, ");
+			}
+			Quarters =(int) (changetotal  / 25 );
+			changetotal = changetotal % 25; 
+			if (Quarters > 0)
+				
+			{
+				System.out.println(Quarters + " quarters, ");
+			}
+			Dimes = (int) (changetotal / 10);
+			changetotal = changetotal % 10;
+			if (Dimes > 0)
+			{
+				System.out.printf("%.0f", Dimes);
+				System.out.println(" dimes");			}
+			Nickels = (int) (changetotal / 5);
+			changetotal = changetotal % 5; 
+			if (Nickels > 0)
+			{
+				System.out.printf("%.0f", Nickels);
+				System.out.println(" nickels");			}
+			Pennies = (int) (changetotal / 1);
+			changetotal = changetotal % 1;
+			if (Pennies > 0)
+			{
+				System.out.printf("%.0f", Pennies);
+				System.out.println(" pennies");
+			}
+
+			
+			
+			
 		}
 		else if (cash == price) {
 			System.out.println("Thanks Have a nice day!");
-//			break;
 		}
 	}
 }
+
+
